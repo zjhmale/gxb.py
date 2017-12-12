3c6eff1c7db7822e3c80b21743112d72b109abfb
zjhmale <zjhmale@gmail.com>
2017-12-12 17:07:01 +0800

    Add tox support

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='gxb.py',
+    version='0.1.0',
+    packages=['gxb'],
+    description='GXB API Wrapper',
+    url='https://github.com/zjhmale/gxb.py',
+    author='zjhmale',
+    license='MIT',
+    author_email='zjhsdtc@gmail.com',
+    install_requires=['requests', 'pyOpenSSL', 'ndg-httpsclient', 'pyasn1'],
+    keywords='GXB GXShare',
+    classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
